870ec27ffb416d8f995e856c6464ea30cb6d8fb1
ChoiByeol <honey333888@naver.com>
2023-01-12 02:10:49 +0900

    이메일, 비밀번호 유효성 검사

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,16 +1,13 @@
 import api from '@/Api/api';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import Input from './Input';
 import Logo from './Logo';
 
 const Login = () => {
   const [account, setAccount] = useState({
-    id: '',
+    email: '',
     password: '',
   });
-
-  const { register, handleSubmit } = useForm();
 
   const onChangeInputValue = (e: any) => {
     const { name, value } = e.target;
@@ -21,14 +18,14 @@ const Login = () => {
   };
 
   const onClickLoginButton = async () => {
-    const { id, password } = account;
+    const { email, password } = account;
     // admin@programmers.co.kr
     // programmers
     const login = await api
       .post(
         `/login`,
         {
-          email: id,
+          email: email,
           password: password,
         },
         {
@@ -43,6 +40,17 @@ const Login = () => {
       });
   };
 
+  type formState = {
+    email: string;
+    password: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formState>();
+
   return (
     <center>
       <form onSubmit={handleSubmit(onClickLoginButton)}>
@@ -50,19 +58,45 @@ const Login = () => {
         <div className="absolute left-3.5">뒤로가기</div>
         <Logo logoText="로그인" />
         <br />
-        <Input
-          inputName="이메일"
-          inputType="id"
-          placeholder="이메일을 입력해 주세요."
-          onChangeInputValue={onChangeInputValue}
-        />
+        <div className="form-control w-2/5 min-w-[250px]">
+          <div className="text-sm">이메일</div>
+          <input
+            {...register('email', {
+              required: '이메일 입력은 필수입니다.',
+              pattern: {
+                value: /^[A-Za-z0-9+-_.]+@[A-Za-z]+\.com$|\.co.kr$/,
+                message: '정확한 이메일 주소를 입력해 주세요.',
+              },
+            })}
+            type="text"
+            name="email"
+            value={account.email}
+            placeholder="이메일을 입력해 주세요."
+            className="input input-bordered text-center"
+            onChange={onChangeInputValue}
+          />
+        </div>
+        <span className="label-text-alt text-red-500">{errors?.email?.message}</span>
         <br />
-        <Input
-          inputName="비밀번호"
-          inputType="password"
-          placeholder="비밀번호를 입력해 주세요."
-          onChangeInputValue={onChangeInputValue}
-        />
+        <div className="form-control w-2/5 min-w-[250px]">
+          <div className="text-sm">비밀번호</div>
+          <input
+            {...register('password', {
+              required: '비밀번호 입력은 필수입니다.',
+              minLength: {
+                value: 8,
+                message: '8자리 이상의 비밀번호를 입력해 주세요.',
+              },
+            })}
+            type="text"
+            name="password"
+            value={account.password}
+            placeholder="비밀번호를 입력해 주세요."
+            className="input input-bordered text-center"
+            onChange={onChangeInputValue}
+          />
+        </div>
+        <span className="label-text-alt text-red-500">{errors?.password?.message}</span>
         <br />
         <button
           type="submit"
@@ -70,7 +104,6 @@ const Login = () => {
         >
           로그인
         </button>
-        <br />
       </form>
     </center>
   );
