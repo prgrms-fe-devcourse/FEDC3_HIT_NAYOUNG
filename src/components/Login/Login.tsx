812873a62076ec8ee7e2314c53994d322bbb71c6
ChoiByeol <honey333888@naver.com>
2023-01-12 01:19:24 +0900

    버튼 색 상수로 수정

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,6 @@
 import api from '@/Api/api';
 import { useState } from 'react';
-import Button from './Button';
+import { useForm } from 'react-hook-form';
 import Input from './Input';
 import Logo from './Logo';
 
@@ -9,6 +9,8 @@ const Login = () => {
     id: '',
     password: '',
   });
+
+  const { register, handleSubmit } = useForm();
 
   const onChangeInputValue = (e: any) => {
     const { name, value } = e.target;
@@ -37,33 +39,39 @@ const Login = () => {
       )
       .then((res) => {
         const token = res.data.token;
-        localStorage.setItem(id, token);
+        localStorage.setItem('login-token', token);
       });
   };
 
   return (
     <center>
-      <br />
-      <div className="absolute left-3.5">뒤로가기</div>
-      <Logo logoText="로그인" />
-      <br />
-      <Input
-        inputName="이메일"
-        inputType="id"
-        placeholder="이메일을 입력해 주세요."
-        onChangeInputValue={onChangeInputValue}
-      />
-      <br />
-      <Input
-        inputName="비밀번호"
-        inputType="password"
-        placeholder="비밀번호를 입력해 주세요."
-        onChangeInputValue={onChangeInputValue}
-      />
-      <br />
-      <Button buttonText="로그인" onClickButton={onClickLoginButton} />
-      <br />
-      {/* <Button buttonText="회원가입" */}
+      <form onSubmit={handleSubmit(onClickLoginButton)}>
+        <br />
+        <div className="absolute left-3.5">뒤로가기</div>
+        <Logo logoText="로그인" />
+        <br />
+        <Input
+          inputName="이메일"
+          inputType="id"
+          placeholder="이메일을 입력해 주세요."
+          onChangeInputValue={onChangeInputValue}
+        />
+        <br />
+        <Input
+          inputName="비밀번호"
+          inputType="password"
+          placeholder="비밀번호를 입력해 주세요."
+          onChangeInputValue={onChangeInputValue}
+        />
+        <br />
+        <button
+          type="submit"
+          className="btn w-2/5 min-w-[250px] bg-BASE border-BASE hover:bg-HOVER hover:border-HOVER"
+        >
+          로그인
+        </button>
+        <br />
+      </form>
     </center>
   );
 };
