a06e68991b92f30016e12b4aa0b674075559954b
suhwa <hansuhwa2005@gmail.com>
2023-01-12 00:07:12 +0900

    폰트 적용, 라우터 URL 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { Home, Login, Signup, ReviewList, ReviewDetail, NotFound } from './pages';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  HOME_PAGE,
+  LOGIN_PAGE,
+  NOT_FOUND_PAGE,
+  REVIEW_DETAIL_PAGE,
+  REVIEW_LIST_PAGE,
+  SIGNUP_PAGE,
+} from '@/utils/constants';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/:category" element={<ReviewList />} />
-        <Route path="/:category/detail" element={<ReviewDetail />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path={HOME_PAGE} element={<Home />} />
+        <Route path={LOGIN_PAGE} element={<Login />} />
+        <Route path={SIGNUP_PAGE} element={<Signup />} />
+        <Route path={REVIEW_LIST_PAGE} element={<ReviewList />} />
+        <Route path={REVIEW_DETAIL_PAGE} element={<ReviewDetail />} />
+        <Route path={NOT_FOUND_PAGE} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+// router
+export const HOME_PAGE = '/';
+export const LOGIN_PAGE = '/login';
+export const SIGNUP_PAGE = '/signup';
+export const REVIEW_LIST_PAGE = '/category/:category';
+export const REVIEW_DETAIL_PAGE = '/category/:category/detail';
+export const NOT_FOUND_PAGE = '*';
