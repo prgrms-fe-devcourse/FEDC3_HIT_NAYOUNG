207eab8c7f62fce1f7ecb57c700ea891ceec3819
parkmoen <pmj6516@naver.com>
2023-01-13 00:04:52 +0900

    리뷰 게시글 컴포넌트와 카테고리 컴포넌트 조립하기 (UI 로직)

--- src/components/Home/Category/CategoryItem.tsx
+++ src/components/Home/Category/CategoryItem.tsx
@@ -6,6 +6,17 @@ import { BsKeyboard } from 'react-icons/bs';
 import { Category, CategoryNameAndIcon } from '@/types';
 import { Link } from 'react-router-dom';
 import { REVIEW_LIST_PAGE } from '@/utils/constants';
+import styled from '@emotion/styled';
+
+// tailwind css에 원하는 percentage가 존재하지 않아서 @emotion을 사용했니다.
+// 더 좋은 방법이 있다면 의견 남겨주세요.
+const StyledLink = styled(Link)`
+  width: 47.5%;
+
+  @media (min-width: 768px) {
+    width: 30%;
+  }
+`;
 
 const categoryIcon: CategoryNameAndIcon = {
   노트북: AiOutlineLaptop,
@@ -20,9 +31,9 @@ const CategoryItem = ({ name, posts }: Category) => {
   const Icon = categoryIcon[name];
 
   return (
-    <Link to={`/${REVIEW_LIST_PAGE}`}>
-      <section className="w-36 m-1 flex flex-col items-start p-2.5 text-TEXT_BASE_BLACK border-BASE border rounded-xl cursor-pointer">
-        <div className="border-BASE border-2 rounded-full p-2">
+    <StyledLink to={`/${REVIEW_LIST_PAGE}`}>
+      <section className="w-full flex flex-col items-start p-2.5 text-TEXT_BASE_BLACK border-BASE border rounded-xl cursor-pointer">
+        <div className="border-BASE border-1 rounded-full p-2">
           <Icon className="text-xl" />
         </div>
         <div className="text-lg">{name}</div>
@@ -30,7 +41,7 @@ const CategoryItem = ({ name, posts }: Category) => {
           {posts.length ? posts.length : 0} reviews
         </span>
       </section>
-    </Link>
+    </StyledLink>
   );
 };
 
