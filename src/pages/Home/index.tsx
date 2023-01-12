1ec97d87d1f22410ce7c9201b17a40e3dd66323b
suhwa <hansuhwa2005@gmail.com>
2023-01-12 14:27:40 +0900

    만든 컴포넌트 테스트용 코드 지움(브라우저에서 체크하려고, home에 정의해둔 것)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,7 @@
-import CategoryList from '@/components/Home/Category/CategoryList';
-
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <CategoryList />
     </div>
   );
 };
