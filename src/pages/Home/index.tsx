e6097fedc6b1860a1972f6e533163e8bcfb951b7
parkmoen <pmj6516@naver.com>
2023-01-13 23:31:03 +0900

    특정 채널의 포스트 목록 API + 채널 목록 API 동시에 데이터를 받아와서 화면에 보여주는 비지니스 로직 구현

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,51 +3,110 @@ import { InformLoginModal } from '@/components/Modal';
 import { informLoginModalState } from '@/store/store';
 import ReviewPoster from '@/components/Home/ReviewPoster';
 import CategoryList from '@/components/Home/Category/CategoryList';
+import { useEffect, useState } from 'react';
+import { Category, CategoryName } from '@/types';
+import { getCategory } from '@/Api/category';
+import { getSpecifiedReviewPoster } from '@/Api/reviewPoster';
 
-// UI test를 위한 더미 데이터
-const DUMMY_DATA = {
-  title: '맥북 강',
-  image:
-    'https://res.cloudinary.com/learnprogrammers/image/upload/v1673336085/post/6d8e6f95-d28a-4a47-bfe3-833bebd3ec98.jpg',
-  _id: '63bd151693836272216d3256',
-};
+type DataType = {
+  category: Category[];
+  specifiedPoster: {
+    id: string;
+    title: string;
+    image: string;
+  }[];
+} | null;
+
+const validCategoryName: CategoryName[] = [
+  '노트북',
+  '모니터',
+  '시계',
+  '오디오',
+  '키보드',
+  '휴대폰',
+];
 
 const Home = () => {
   const [open, setOpen] = useRecoilState(informLoginModalState);
+  const [data, setData] = useState<DataType>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(false);
   const titleClassName =
     'text-start sm:text-base md:text-lg lg:text-xl text-TEXT_BASE_BLACK font-semibold mb-2';
 
-  return (
-    <>
-      {/* 모바일 뷰에서만 보이는 임시 Logo 컴포넌트 레이아웃  */}
-      <h1
-        className={`md:hidden absolute top-5 left-1/2 -translate-x-1/2 text-5xl text-BASE font-extrabold`}
-      >
-        HIT
-      </h1>
-      <section className="flex flex-col items-center md:items-start lg:items-start max-w-xl w-full mx-auto pt-24 lg:pt-10 md:pt-10">
-        {/* 추천 게시글 area */}
-        <div className="w-11/12 h-full">
-          <h2 className={titleClassName}>추천 리뷰 게시글</h2>
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const getAllMainData = (
+          await Promise.all([getCategory(), getSpecifiedReviewPoster()])
+        ).map(({ data }) => data);
+        const categoryResponse: Category[] = getAllMainData[0];
+        const specifiedReviewPosterResponse: {
+          id: string;
+          title: string;
+          image: string;
+        }[] = getAllMainData[1];
+        const validCategory = categoryResponse.filter((category) =>
+          validCategoryName.includes(category.name)
+        );
+        const validSpecifiedReviewPosterResponse = specifiedReviewPosterResponse.slice(
+          0,
+          2
+        );
 
-          <ReviewPoster
-            id={DUMMY_DATA._id}
-            title={DUMMY_DATA.title}
-            image={DUMMY_DATA.image}
-          />
-          <ReviewPoster
-            id={DUMMY_DATA._id}
-            title={DUMMY_DATA.title}
-            image={DUMMY_DATA.image}
-          />
-          {/* 카테고리 목록 area */}
-          <h2 className={titleClassName}>카테고리</h2>
-          <CategoryList />
-        </div>
-      </section>
-      <InformLoginModal />
-    </>
-  );
+        setData({
+          category: validCategory,
+          specifiedPoster: validSpecifiedReviewPosterResponse,
+        });
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    run();
+  }, []);
+
+  if (loading) {
+    // 로딩 컴포넌트(스켈레톤)으로 대체
+    return null;
+  }
+
+  if (data) {
+    return (
+      <>
+        <h1
+          className={`md:hidden absolute top-5 left-1/2 -translate-x-1/2 text-5xl text-BASE font-extrabold`}
+        >
+          HIT
+        </h1>
+        <section className="flex flex-col items-center md:items-start lg:items-start max-w-xl w-full mx-auto pt-24 lg:pt-10 md:pt-10">
+          {/* 추천 게시글 area */}
+          <div className="w-11/12 h-full">
+            <h2 className={titleClassName}>추천 리뷰 게시글</h2>
+
+            <ReviewPoster
+              id={data?.specifiedPoster[0].id}
+              title={data?.specifiedPoster[0].title}
+              image={data?.specifiedPoster[0].image}
+            />
+            <ReviewPoster
+              id={data?.specifiedPoster[1].id}
+              title={data?.specifiedPoster[1].title}
+              image={data?.specifiedPoster[1].image}
+            />
+            {/* 카테고리 목록 area */}
+            <h2 className={titleClassName}>카테고리</h2>
+            <CategoryList category={data?.category} />
+          </div>
+        </section>
+        <InformLoginModal />
+      </>
+    );
+  }
 };
 
 export default Home;
