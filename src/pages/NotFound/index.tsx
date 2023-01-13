df9d72dca45955efe6f47f1a5f457a496dd1071c
ChoiByeol <honey333888@naver.com>
2023-01-14 01:49:36 +0900

    404 페이지 홈 이동 버튼 추가

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,14 +1,22 @@
+import { HOME_PAGE } from '@/utils/constants';
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
     <center className="overflow-hidden">
-      <div className="text-2xl mt-20 italic">404 Page</div>
-      <div className="box-border rounded-404 w-80 h-80 mt-10 bg-[#FFC7C7] flex items-center justify-center">
+      <div className="text-2xl mt-10 italic">404 Page</div>
+      <div className="box-border rounded-404 w-80 h-80 mt-10 bg-BASE flex items-center justify-center">
         <div className="text-white text-6xl">HIT</div>
       </div>
-      <div className="text-xl mt-16">
+      <div className="text-xl mt-10">
         죄송합니다.
         <p>페이지를 찾을 수 없습니다.</p>
       </div>
+      <Link to={HOME_PAGE}>
+        <button className="btn mt-10 w-72 bg-white border-BASE text-BASE hover:bg-white hover:border-HOVER hover:text-HOVER">
+          BACK TO HOME
+        </button>
+      </Link>
     </center>
   );
 };
