b68ce57493e5810c0dade46c9487f08aa70dd2af
ChoiByeol <honey333888@naver.com>
2023-01-14 01:28:27 +0900

    404 페이지 구현

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,8 +1,15 @@
 const NotFound = () => {
   return (
-    <div>
-      <h1>페이지를 찾을 수 없습니다.</h1>
-    </div>
+    <center className="overflow-hidden">
+      <div className="text-2xl mt-20 italic">404 Page</div>
+      <div className="box-border rounded-404 w-80 h-80 mt-10 bg-[#FFC7C7] flex items-center justify-center">
+        <div className="text-white text-6xl">HIT</div>
+      </div>
+      <div className="text-xl mt-16">
+        죄송합니다.
+        <p>페이지를 찾을 수 없습니다.</p>
+      </div>
+    </center>
   );
 };
 
