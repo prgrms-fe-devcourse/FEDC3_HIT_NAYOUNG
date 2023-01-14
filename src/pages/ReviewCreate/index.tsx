271bed9dc6ea50094566b7538beb8b88a6ae3cf0
suhwa <hansuhwa2005@gmail.com>
2023-01-14 15:34:49 +0900

    리뷰 생성 페이지 반응형 마진 제거

--- src/pages/ReviewCreate/index.tsx
+++ src/pages/ReviewCreate/index.tsx
@@ -2,7 +2,7 @@ import ReviewCreateForm from '@/components/ReviewCreateForm';
 
 const ReviewCreate = () => {
   return (
-    <div className="h-full xl:ml-60 md:ml-16 pt-16">
+    <div className="h-full pt-16">
       <ReviewCreateForm />
     </div>
   );
