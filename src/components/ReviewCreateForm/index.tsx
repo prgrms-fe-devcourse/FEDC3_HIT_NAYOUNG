0fd801414ff93ec8726ced33c7dab6f2d435e63f
suhwa <hansuhwa2005@gmail.com>
2023-01-16 23:33:40 +0900

    리뷰생성 페이지 > 취소 | 리뷰제출 버튼 클릭 시 모달 연결
    1. 취소버튼 누를 경우, 현재 작성된 내용 사라진다는 warning 모달 띄움
    
    2. 리뷰제출 시, 리뷰생성 api, 연결하는 동안 리뷰 생성하는 중인걸 알려주기위해 모달 띄움

--- src/components/ReviewCreateForm/index.tsx
+++ src/components/ReviewCreateForm/index.tsx
@@ -4,8 +4,10 @@ import { ReviewFormData } from '@/types';
 import RegisterTextarea from '@/components/ReviewCreateForm/RegisterTextarea';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import Button from '@/components/ReviewCreateForm/Button';
-import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useCallback, useState } from 'react';
+import InformCreateLoadingModal from '@/components/ReviewCreateForm/InformCreateLoadingModal';
+import InformCancelModal from '@/components/ReviewCreateForm/InformCancelModal';
 
 // FIXME: 비동기 로직, 컴포넌트랑 분리
 
@@ -15,10 +17,22 @@ const ReviewCreateForm = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<ReviewFormData>();
-
+  const prevCategoryUrl = '/category/오디오'; // FIXME: 더미데이터 <- 이전 페이지의 카테고리 값(id, name)받아와야함
+  const prevCategoryState = { id: '63bd141d93836272216d324a', name: '오디오' };
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [isOpenCancelModal, setIsOpenCancelModal] = useState(false);
+
+  const onClickCancelButton = useCallback(() => {
+    setIsOpenCancelModal(true);
+  }, []);
+
+  const onOffCancelModal = useCallback(() => {
+    setIsOpenCancelModal(false);
+  }, []);
 
   const onSubmitReview: SubmitHandler<ReviewFormData> = ({ title, contents, image }) => {
+    setLoading(true);
     const titleAndContent = {
       title,
       contents,
@@ -37,69 +51,74 @@ const ReviewCreateForm = () => {
           'Content-Type': 'multipart/form-data',
         },
       })
-      .then(() => navigate(`/category/오디오`));
+      .then(() =>
+        navigate(prevCategoryUrl, {
+          state: prevCategoryState,
+        })
+      )
+      .finally(() => setLoading(false));
     // TODO: 현재 어떤 카테고리에서 글 생성했는지 받아와야함. 글 생성 후, 다시 그 카테고리 페이지로 이동해야함. 현재는 오디오로 지정해둠
   };
 
-  const onClickCancelButton = useCallback(() => {
-    // TODO: 취소, 리뷰남기기 -> 나올 모달 만들고, 연결
-  }, []);
-
   return (
-    <div className="flex justify-center text-TEXT_BASE_BLACK">
-      <div className="flex flex-col w-5/12 max-md:w-9/12">
-        <h1 className="text-lg pb-2.5 self-start">리뷰작성</h1>
-        <form className="flex flex-col">
-          <RegisterInput
-            type="text"
-            placeholder="제목"
-            style={{
-              container: 'mb-4 flex flex-col',
-              input: 'input w-full bg-GRAY_100',
-            }}
-            register={register}
-            registerName="title"
-            registerRules={{ required: '제목을 입력해주세요' }}
-            errors={errors.title}
-          />
-          <RegisterTextarea
-            rows={10}
-            placeholder="사용후기를 남겨주세요"
-            style={{
-              container: 'flex flex-col mb-4',
-              textarea: 'textarea bg-GRAY_100 text-sm',
-            }}
-            register={register}
-            registerName="contents"
-            registerRules={{ required: '내용을 입력해주세요' }}
-            errors={errors.contents}
-          />
-          <RegisterInput
-            type="file"
-            style={{
-              container: 'flex flex-col mb-4',
-            }}
-            accept="image/*"
-            register={register}
-            registerName="image"
-            registerRules={{ required: '1장의 이미지를 올려주세요' }}
-            errors={errors.image}
-          />
-          <div className="action-buttons flex gap-4">
-            <Button
-              name="취소"
-              style="bg-GRAY_200 p-2.5 flex-1 rounded-xl hover:bg-slate-300"
-              clickHandler={onClickCancelButton}
+    <>
+      <div className="flex justify-center text-TEXT_BASE_BLACK">
+        <div className="flex flex-col w-5/12 max-md:w-9/12">
+          <h1 className="text-lg pb-2.5 self-start">리뷰작성</h1>
+          <form className="flex flex-col">
+            <RegisterInput
+              type="text"
+              placeholder="제목"
+              style={{
+                container: 'mb-4 flex flex-col',
+                input: 'input w-full bg-GRAY_100',
+              }}
+              register={register}
+              registerName="title"
+              registerRules={{ required: '제목을 입력해주세요' }}
+              errors={errors.title}
             />
-            <Button
-              name="리뷰남기기"
-              style="bg-BASE p-2.5 flex-1 rounded-xl hover:bg-HOVER"
-              clickHandler={handleSubmit(onSubmitReview)}
+            <RegisterTextarea
+              rows={10}
+              placeholder="사용후기를 남겨주세요"
+              style={{
+                container: 'flex flex-col mb-4',
+                textarea: 'textarea bg-GRAY_100 text-sm',
+              }}
+              register={register}
+              registerName="contents"
+              registerRules={{ required: '내용을 입력해주세요' }}
+              errors={errors.contents}
             />
-          </div>
-        </form>
+            <RegisterInput
+              type="file"
+              style={{
+                container: 'flex flex-col mb-4',
+              }}
+              accept="image/*"
+              register={register}
+              registerName="image"
+              registerRules={{ required: '1장의 이미지를 올려주세요' }}
+              errors={errors.image}
+            />
+            <div className="action-buttons flex gap-4">
+              <Button
+                name="취소"
+                style="bg-GRAY_200 p-2.5 flex-1 rounded-xl hover:bg-slate-300"
+                clickHandler={onClickCancelButton}
+              />
+              <Button
+                name="리뷰남기기"
+                style="bg-BASE p-2.5 flex-1 rounded-xl hover:bg-HOVER"
+                clickHandler={handleSubmit(onSubmitReview)}
+              />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      <InformCancelModal isOpen={isOpenCancelModal} modalOffHandler={onOffCancelModal} />
+      <InformCreateLoadingModal isOpen={loading} />
+    </>
   );
 };
 
