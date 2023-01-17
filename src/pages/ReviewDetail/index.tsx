6318b5c9a632bcd033cbd93cee90078dcc80bdc4
JooChunWook <jha970107@gmail.com>
2023-01-17 15:28:39 +0900

    2Depth 이상 prop전달 -> Recoil 상태 관리로 변경

--- src/pages/ReviewDetail/index.tsx
+++ src/pages/ReviewDetail/index.tsx
@@ -2,7 +2,7 @@ import ReviewCommentInput from '@/components/ReviewDetail/ReviewComment/ReviewCo
 import ReviewCommentList from '@/components/ReviewDetail/ReviewComment/ReviewCommentList';
 import ReviewContent from '@/components/ReviewDetail/ReviewContent';
 import { callGetReviewDetailAPI } from '@/Api/reviewDetail';
-import { breadCrumbState, createCommentState, likePropState } from '@/store/store';
+import { createCommentState, likePropState, reviewContentState } from '@/store/store';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
@@ -19,10 +19,10 @@ const ReviewDetail = () => {
 
   // 댓글 관련 state
   const [commentList, setCommentList] = useState();
-  const createComment = useRecoilValue(createCommentState);
+  const createdComment = useRecoilValue(createCommentState);
 
-  // breadCrumb (channelId, category)
-  const breadCrumbIdCategoryState = useSetRecoilState(breadCrumbState);
+  // 상세페이지 데이터 2depth이상 전역 상태 관리
+  const setReviewContentHandler = useSetRecoilState(reviewContentState);
 
   // 좋아요 state 부모로 전달
   const setLikeState = useSetRecoilState(likePropState);
@@ -32,15 +32,17 @@ const ReviewDetail = () => {
       try {
         const data = await callGetReviewDetailAPI(id);
         const user = await getUserId();
-        console.log(data);
-        console.log(user);
 
         setUserId(user._id);
         setReviewContent(data);
         setCommentList(data.comments);
-        breadCrumbIdCategoryState({
-          channelId: data.channel._id,
-          category: data.channel.name,
+        setReviewContentHandler({
+          userId: user._id,
+          author: data.author,
+          title: data.title,
+          image: data.image,
+          createdAt: data.createdAt,
+          channel: data.channel,
         });
         setLikeState({ likes: data.likes, id });
       } catch (error) {
@@ -48,19 +50,13 @@ const ReviewDetail = () => {
       }
     };
     getReviewDetail();
-  }, [createComment]);
+  }, [createdComment]);
 
   return (
     <div className="max-w-xl w-full my-0 mx-auto h-full px-8 pt-8">
       {reviewContent && (
         <>
-          <ReviewContent
-            userId={userId}
-            author={reviewContent.author}
-            title={reviewContent.title}
-            image={reviewContent.image}
-            createdAt={reviewContent.createdAt}
-          />
+          <ReviewContent title={reviewContent.title} image={reviewContent.image} />
           <ReviewCommentInput postId={id} />
           {commentList && <ReviewCommentList commentList={commentList} />}
         </>
