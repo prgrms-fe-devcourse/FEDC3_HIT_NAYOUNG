8f1bde2e166886e23cf1244fa23ccd9b816e1906
ChoiByeol <honey333888@naver.com>
2023-01-20 14:20:46 +0900

    로그인, 회원가입 페이지 모달창 추가

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,9 +1,11 @@
 import SignupForm from '@/components/Auth/SignUp';
+import { InformLoginModal } from '@/components/Modal';
 
 const Signup = () => {
   return (
     <div>
       <SignupForm />
+      <InformLoginModal />
     </div>
   );
 };
