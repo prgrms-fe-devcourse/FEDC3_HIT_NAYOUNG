a80cc68e7dfcdf196a3f0dd93e0a25ffbd6a51e1
ChoiByeol <honey333888@naver.com>
2023-01-20 15:50:49 +0900

    로그인 상태 아닌 경우 안 보이도록 처리

--- src/components/Home/UserList/UserList.tsx
+++ src/components/Home/UserList/UserList.tsx
@@ -1,12 +1,13 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { MY_PAGE } from '@/utils/constants';
+import { LOGIN_TOKEN, MY_PAGE } from '@/utils/constants';
 
 import { getUserInformation, getUserNameList } from '@/Api/user';
 
 import { BiUserCircle } from 'react-icons/bi';
 import { UserItem } from './UserItem';
+import { getLocalStorage } from '@/utils/storage';
 
 type UserNameListProps = {
   name: string;
@@ -19,43 +20,53 @@ const UserList = () => {
   const navigate = useNavigate();
 
   const [myName, setMyName] = useState();
+  const [authState, setAuthState] = useState(false);
   const [userNameList, setUserNameList] = useState<UserNameList>();
-
-  useEffect(() => {
-    const callGetUserInformation = async () => {
-      const response = await getUserInformation();
-      setMyName(response.fullName);
-    };
-
-    const callgetUserNameList = async () => {
-      const userNameList = await getUserNameList();
-      setUserNameList(userNameList);
-    };
-
-    callGetUserInformation();
-    callgetUserNameList();
-  }, []);
 
   const moveMyProfile = useCallback(() => {
     navigate(MY_PAGE);
   }, []);
 
-  return (
-    <div className="w-48 border border-BASE rounded-md shadow-sm pb-1">
-      <div
-        className="flex hover:bg-slate-50 cursor-pointer mt-1 p-1 px-2"
-        onClick={moveMyProfile}
-      >
-        <BiUserCircle size={24} className="mr-1" />
-        <div className="ml-1">{myName}</div>
+  useEffect(() => {
+    (async () => {
+      const isLogIn = getLocalStorage(LOGIN_TOKEN);
+
+      if (isLogIn) setAuthState(true);
+      else setAuthState(false);
+    })();
+
+    const callGetUserInformation = async () => {
+      const response = await getUserInformation();
+      setMyName(response.fullName);
+    };
+
+    const callGetUserNameList = async () => {
+      const userNameList = await getUserNameList();
+      setUserNameList(userNameList);
+    };
+
+    callGetUserInformation();
+    callGetUserNameList();
+  }, []);
+
+  if (authState)
+    return (
+      <div className="w-48 border border-BASE rounded-md shadow-sm pb-1">
+        <div
+          className="flex hover:bg-slate-50 cursor-pointer mt-1 p-1 px-2"
+          onClick={moveMyProfile}
+        >
+          <BiUserCircle size={24} className="mr-1" />
+          <div className="ml-1">{myName}</div>
+        </div>
+        <div className="border border-t-BASE border-b-white mt-1"></div>
+        {userNameList &&
+          userNameList.map((user: UserNameListProps) => {
+            return <UserItem userName={user.name} id={user.id} key={user.id} />;
+          })}
       </div>
-      <div className="border border-t-BASE border-b-white mt-1"></div>
-      {userNameList &&
-        userNameList.map((user: UserNameListProps) => {
-          return <UserItem userName={user.name} id={user.id} key={user.id} />;
-        })}
-    </div>
-  );
+    );
+  return <></>;
 };
 
 export default UserList;
