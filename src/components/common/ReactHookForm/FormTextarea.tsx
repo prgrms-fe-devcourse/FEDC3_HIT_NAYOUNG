4aa447921f78e000d3e7ac6b91f7013264ba7f61
suhwa <hansuhwa2005@gmail.com>
2023-01-25 01:38:29 +0900

    재사용 컴포넌트(FormInput, FormTextarea) 주석달기

--- src/components/common/ReactHookForm/FormTextarea.tsx
+++ src/components/common/ReactHookForm/FormTextarea.tsx
@@ -1,6 +1,14 @@
 import ErrorMessage from '@/components/ReviewCreateForm/ErrorMessage';
 import { FormTextareaProps } from '@/types';
 
+/**
+ * FormTextarea 쓸 때, register에서 관리하는 값의 타입을 넘겨주세요. ex) FormTextarea<FormValueType>
+ * @register useForm의 리턴값
+ * @registerName register에서 관리할 key값
+ * @registerRules 해당 input value를 관리할 때 규칙(required, pattern..)
+ * @errors useForm > formState > errors 중 해당 registerName의 에러 | 넘기면, 에러메시지 보여줌
+ * @etc 그 외는 textarea attribute
+ */
 function FormTextarea<FormValues>({
   rows,
   placeholder,
